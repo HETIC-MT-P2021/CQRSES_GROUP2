d04b1e2c3b4e4bac776542befc8f0f149ffaf2d6
Akecel <axel.rayer@gmail.com>
2021-02-01 14:05:15 +0100

    update CreateArticleMethod for Author ID implementation

--- controllers/Article.go
+++ controllers/Article.go
@@ -6,12 +6,27 @@ import (
 	"cqrses/models"
 	"net/http"
 
+	"github.com/dgrijalva/jwt-go"
+	validation "github.com/go-ozzo/ozzo-validation/v4"
 	"github.com/labstack/echo/v4"
 )
 
 // CreateArticle create an article
 func CreateArticle(c echo.Context) error {
-	var articleStore models.ArticleStore
+	articleStore := models.ArticleStore{}
+
+	if err := validation.ValidateStruct(&articleStore,
+		validation.Field(&articleStore.Title, validation.Required),
+		validation.Field(&articleStore.Content, validation.Required),
+	); err != nil {
+		return c.JSON(http.StatusBadRequest, SetResponse(http.StatusBadRequest, "Validation error", err.Error()))
+	}
+
+	user := c.Get("user").(*jwt.Token)
+	claims := user.Claims.(jwt.MapClaims)
+	id := claims["id"].(uint64)
+
+	articleStore.AuthorID = id
 
 	command := article.CreateArticleCommand{
 		ArticleStore: articleStore,
